c1283ac6b3f5a028578b010cd30823db7ade77f5
Jonathan <jonathanatss@gmail.com>
2026-08-12 21:40:49 -0300

    Corrige titulos das variantes do Pablo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "fitplan-v4";
-const RUNTIME_CACHE = "fitplan-runtime-v4";
+const CACHE_NAME = "fitplan-v5";
+const RUNTIME_CACHE = "fitplan-runtime-v5";
 
 const APP_SHELL = [
   "./",
